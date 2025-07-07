4fe7125fd25d8d4493220224cd1c8fe63372079d
Leni Aniva <v@leni.sh>
2025-07-07 15:45:18 -0700

    fix: Set event loop when there isn't one

--- pantograph/utils.py
+++ pantograph/utils.py
@@ -6,7 +6,9 @@ def get_event_loop():
     try:
         return asyncio.get_running_loop()
     except RuntimeError:
-        return asyncio.new_event_loop()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        return loop
 
 def to_sync(func):
     loop = get_event_loop()
