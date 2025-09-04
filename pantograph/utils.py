7312a350cc50c446dce5e43ecb55bded8cf45962
Leni Aniva <a@leni.sh>
2025-09-04 14:10:26 -0700

    feat: Only use one event loop

--- pantograph/utils.py
+++ pantograph/utils.py
@@ -2,12 +2,13 @@ import asyncio
 from pathlib import Path
 import functools as F
 
+DEFAULT_EVENT_LOOP = asyncio.new_event_loop()
+
 def get_event_loop():
     try:
         return asyncio.get_event_loop()
     except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        asyncio.set_event_loop(DEFAULT_EVENT_LOOP)
         return loop
 
 def to_sync(func):
